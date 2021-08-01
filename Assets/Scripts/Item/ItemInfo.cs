ca8855bddb0b9a7b208fadd4b99c9462e2b3fac6
akatsuki774 <akatsuki774@users.noreply.github.com>
2021-08-02 01:08:03 +0900

    ItemのScriptableObjectを作成

---
+++ Assets/Scripts/Item/ItemInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ItemInfo : ScriptableObject
+{
+    public enum Type
+    {
+        HPRecovery,
+        Weapon,
+        Armor,
+    }
+
+    [SerializeField]
+    Type ItemType { get; } = Type.HPRecovery;
+
+    [SerializeField]
+    string Name { get; } = "";
+
+    [SerializeField]
+    string SortName { get; } = "";
+
+    [SerializeField]
+    string Detail { get; } = "";
+
+    [SerializeField]
+    int Amount { get; } = 0;
+}
