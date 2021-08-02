2cd98d0ca105e78bc38c4b406e104996b1af93c9
akatsuki774 <akatsuki774@users.noreply.github.com>
2021-08-02 22:22:41 +0900

    Update CharacterStatus.cs

--- Assets/Scripts/Status/CharacterStatus.cs
+++ Assets/Scripts/Status/CharacterStatus.cs
@@ -9,20 +9,9 @@ public abstract class CharacterStatus : ScriptableObject
     // レベル
     [SerializeField]
     int Level { get; set; } = 1;
-    // 最大HP
+    // HP
     [SerializeField]
     int MaxHp { get; set; } = 10;
-    // HP
-    private int _hp = 10;
-    [SerializeField]
-    int Hp 
-    {
-        get { return _hp; }
-        set
-        {
-            _hp = Mathf.Max(0, Mathf.Min(MaxHp, value));
-        }
-    }
     // 力
     [SerializeField]
     int Power { get; set; } = 5;
