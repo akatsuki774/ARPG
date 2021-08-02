6d7ae812ed29e83a3e50a74e408726d56e830566
akatsuki774 <akatsuki774@users.noreply.github.com>
2021-08-03 00:39:22 +0900

    Update CharacterStatus.cs

--- Assets/Scripts/Status/CharacterStatus.cs
+++ Assets/Scripts/Status/CharacterStatus.cs
@@ -7,17 +7,57 @@ using UnityEngine;
 public abstract class CharacterStatus : ScriptableObject
 {
     // 名前
-    string Name { get; set; } = "";
+    [SerializeField]
+    string _name = "";
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    // レベル
+    [SerializeField]
+    int _level = 1;
+    public int Level {
+        get { return _level; }
+        set { _level = value; }
+    }
+    // 最大HP
+    [SerializeField]
+    int _maxHp = 10;
+    public int MaxHp {
+        get { return _maxHp; }
+        set { _maxHp = value; }
+    }
     // HP
     [SerializeField]
-    int MaxHp { get; set; } = 10;
+    int _hp = 10;
+    public int Hp
+    {
+        get { return _hp; }
+        set
+        {
+            _hp = Mathf.Max(0, Mathf.Min(MaxHp, value));
+        }
+    }
     // 力
     [SerializeField]
-    int Power { get; set; } = 5;
+    int _power = 5;
+    public int Power {
+        get { return _power; }
+        set { _power = value; }
+    }
     // 耐久力
     [SerializeField]
-    int Strength { get; set; } = 10;
+    int _strenght = 10;
+    public int Strength {
+        get { return _strenght; }
+        set { _strenght = value; }
+    }
     // 素早さ
     [SerializeField]
-    int Agility { get; set; } = 5;
+    int _agility = 5;
+    public int Agility {
+        get { return _agility; }
+        set { _agility = value; }
+    }
 }
