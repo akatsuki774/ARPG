6ab562e78bc6ea30d8e01d10b94abd9e7e373b28
akatsuki774 <akatsuki774@users.noreply.github.com>
2021-08-02 23:06:38 +0900

    Rename Charactor to CharactorObject

---
+++ Assets/Scripts/CharactorObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharactorObject : MonoBehaviour
+{
+    protected int _currentHp;
+    protected bool _isAlive;
+
+    public abstract int CalcAttackPoint();
+
+    public void AttackOnTarget( CharactorObject target ) {
+        target._currentHp -= this.CalcAttackPoint();
+        if ( target._currentHp <= 0 ) {
+            target._currentHp = 0;
+            target._isAlive = false;
+        }
+    }
+    public bool IsAlive {
+        get { return _isAlive; }
+        set { _isAlive = value; }
+    }
+
+
+    public int CurrentHp {
+        get { return _currentHp; }
+        set { _currentHp = value; }
+    }
+}
